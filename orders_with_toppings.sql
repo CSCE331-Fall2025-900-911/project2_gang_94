89bc505a7c6351c9c8d38ddb2c5aed8f233f0729
dcuina <dan.cuina@outlook.com>
2025-10-01 16:30:33 -0500

    Edited query to better reflect function

--- orders_with_toppings.sql
+++ orders_with_toppings.sql
@@ -1,14 +1,7 @@
 SELECT
-    month,
-    ROUND(AVG(orders_per_month)::numeric, 2) AS avg_toppings_orders
-FROM (
-    SELECT
-        EXTRACT(YEAR FROM orderdate) AS year,
-        EXTRACT(MONTH FROM orderdate) AS month,
-        COUNT(*) AS orders_per_month
-    FROM customers
-    WHERE itemsused LIKE '%add%'
-    GROUP BY year, month
-) AS monthly_count
+    EXTRACT(MONTH FROM orderdate) AS month,
+    COUNT(*)
+FROM customers
+WHERE itemsused LIKE '%add%'
 GROUP BY month
 ORDER BY month;
