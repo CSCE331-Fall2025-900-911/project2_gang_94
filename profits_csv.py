812117a49c33aa4604a0f812733171131e9d3c98
dcuina <dan.cuina@outlook.com>
2025-10-01 14:36:56 -0500

    Fixed error in calculating profits

--- profits_csv.py
+++ profits_csv.py
@@ -5,7 +5,7 @@ from datetime import datetime, timedelta
 data = [['startdate', 'totalsales', 'totalexpenses', 'profitchange']]
 
 sales = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
-
+expenses = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
 
 with open('customers.csv', 'r', newline='') as f_read:
     reader = csv.reader(f_read)
@@ -22,9 +22,10 @@ year = 2024
 for i in range(len(sales)):
     if (sales[month-1] == 0):
         continue
-    iter_arr = [f"{year}-{month}-01", round(sales[month-1],2), random.randint(4000, 5000), 0]
+    expenses[month-1] = random.uniform(4000, 5000)
+    iter_arr = [f"{year}-{month}-01", round(sales[month-1],2), round(expenses[month-1], 2), 0]
     if (sales[month-2] > 0):
-        iter_arr[3] = round(sales[month-1] - sales[month-2],2)
+        iter_arr[3] = round((sales[month-1] - expenses[month-1]) - (sales[month-2] - expenses[month-2]),2)
     data.append(iter_arr)
     month += 1
     if (month > 12):
