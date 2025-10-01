b3399e779076a9080a413ba53c7f6803b873f468
Pratha_Gupta <pratha05@tamu.edu>
2025-10-01 16:30:58 -0500

    Added changes to longest working emplyee

--- Longest_employee.sql
+++ Longest_employee.sql
@@ -1,8 +1,8 @@
---query the find the longest working employee every day  
-SELECT Employee_id, schedule_date, hours_worked, customers_served
+--query the find the longest working employee every day
+SELECT Employee_id, schedule_date, hours_worked
 FROM Employees e1
-WHERE hours_worked = 
+WHERE hours_worked = (
     SELECT MAX(hours_worked)
     FROM Employees e2
-    WHERE e2.schedule_date = e1.schedule_date
+    WHERE e2.schedule_date = e1.schedule_date);
 
