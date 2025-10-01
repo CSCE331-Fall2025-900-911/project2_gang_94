0d8a372d37841fb914f305784560a7a8e391a9cb
ilana-rb <ilana@tamu.edu>
2025-10-01 15:20:38 -0500

    reflects variable rename

--- Longest_employee.sql
+++ Longest_employee.sql
@@ -1,8 +1,8 @@
 --query the find the longest working employee every day  
-SELECT Employee_id, schedule_date, hours_worked, customers_served
+SELECT "Employee ID", schedule_date, "Hours Worked", customers_served
 FROM Employees e1
-WHERE hours_worked = 
-    SELECT MAX(hours_worked)
+WHERE "Hours Worked" = 
+    SELECT MAX("Hours Worked")
     FROM Employees e2
     WHERE e2.schedule_date = e1.schedule_date
 
